8084d4286ed08d7bb7e6ea9a18b4b103d9567b1a
K-Musty <kmustapha9564@gmail.com>
2025-04-03 21:48:15 +0100

    KSS: Instanciating Keyword spotting service from function

--- keyword_spotting_service.py
+++ keyword_spotting_service.py
@@ -6,4 +6,20 @@ SAVED_MODEL_PATH = "model.h5"
 SAMPLES_TO_CONSIDER = 22050
 
 class _Keyword_Spotting_Service:
-    pass
+    model = None
+    _mappings = [
+        "down",
+        "up",
+        "yes",
+        "no",
+        "right",
+        "left"
+    ]
+    _instance = None
+
+
+def Keyword_Spotting_Service():
+
+    # Ensure that we Only have one instance
+    if _Keyword_Spotting_Service._instance is None:
+        _Keyword_Spotting_Service._instance = _Keyword_Spotting_Service()
