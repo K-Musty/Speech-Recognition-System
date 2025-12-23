776437ad0c8ecb5f11b8aab47d4dd0858fb149d0
K-Musty <kmustapha9564@gmail.com>
2025-12-23 22:44:21 +0100

    Server for hading localhost

--- server.py
+++ server.py
@@ -1,14 +1,42 @@
-from flask import Flask
+#!/usr/bin/env python3
+
+from flask import Flask, request, jsonify
+import random
+import os
+from keyword_spotting_service import Keyword_Spotting_Service 
 
 
 app = Flask(__name__)
 
 
+
 @app.route("/predict", methods=['POST'])
+def predict():
+    
+    # Get audio file and save it
+    audio_file = request.files["files"]
+    file_name = str(random.randint(0, 100000))
+    audio_file.save(file_name)
+
+
+    # Invoke the KSS
+    kss = Keyword_Spotting_Service()
+
+
+    # Make a prediction
+    predicted_keyword = kss.predict(file_name)
+
+
+    # Remove the temporary stored audio file
+    os.remove(file_name)
+
+    # Send back the predicted keyword in json
+    data = {"keyword": predicted_keyword}
+
+    return jsonify(data)
 
 
 
 
-
-if __name__ == ("__main__"):
-	app.run(debug=True)
+if __name__ == "__main__":
+	app.run(debug=False)
